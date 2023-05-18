a083a282170f8e517bdc571b6fe0b9c87e148298
Janine Vallente <janinexvallente@gmail.com>
2023-05-18 14:29:33 +0800

    Machine Problem 2

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Button} from "@react-native-material/core";
 
 export default function App() {
   return (
@@ -7,6 +9,7 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
