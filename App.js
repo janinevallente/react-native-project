4ead4a9420bc9bbe155deb75c2812b2aa9d53f0e
Janine Vallente <janinexvallente@gmail.com>
2023-05-18 16:33:39 +0800

    Machine Problem 3

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from "react";
-import {Button} from "@react-native-material/core";
+import React from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 
-export default function App() {
+export default function HomePage() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.logoContainer}>
+        <Text style={styles.logoText}>Welcome to Expo!</Text>
+      </View>
 
+      <TouchableOpacity style={styles.button} onPress={() => alert("Logging in...")}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.button, styles.registerButton]} onPress={() => alert("Registering...")}>
+        <Text style={[styles.buttonText, styles.registerButtonText]}>Register</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#f9f9f9',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    marginBottom: 32,
+  },
+  logoText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  button: {
+    backgroundColor: '#007bff',
+    borderRadius: 24,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    marginVertical: 8,
+    width: 200,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  registerButton: {
+    backgroundColor: '#5cb85c', // Change the color for the register button
+  },
+  registerButtonText: {
+    color: '#fff', // Change the text color for the register button
   },
 });
